21ee5544ab24c1a2e15da181e0e3031a5bd17fa0
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2023-03-22 22:29:12 +0100

    Simple morse code translator

---
+++ dist/morseCode.js
@@ -0,0 +1,78 @@
+"use strict";
+const promptSync = require('prompt-sync')({ sigint: true });
+let input = promptSync("Enter a string to convert to Morse Code: ");
+const morseCodeMap = {
+    'a': '.-',
+    'b': '-...',
+    'c': '-.-.',
+    'd': '-..',
+    'e': '.',
+    'f': '..-.',
+    'g': '--.',
+    'h': '....',
+    'i': '..',
+    'j': '.---',
+    'k': '-.-',
+    'l': '.-..',
+    'm': '--',
+    'n': '-.',
+    'o': '---',
+    'p': '.--.',
+    'q': '--.-',
+    'r': '.-.',
+    's': '...',
+    't': '-',
+    'u': '..-',
+    'v': '...-',
+    'w': '.--',
+    'x': '-..-',
+    'y': '-.--',
+    'z': '--..',
+    '1': '.----',
+    '2': '..---',
+    '3': '...--',
+    '4': '....-',
+    '5': '.....',
+    '6': '-....',
+    '7': '--...',
+    '8': '---..',
+    '9': '----.',
+    '0': '-----',
+    '.': '.-.-.-',
+    ',': '--..--',
+    '?': '..--..',
+    '!': '-.-.--',
+    '-': '-....-',
+    '/': '-..-.',
+    '@': '.--.-.',
+    '(': '-.--.',
+    ')': '-.--.-',
+    '=': '-...-',
+    '+': '.-.-.',
+    '\'': '.----.',
+    '"': '.-..-.',
+    ':': '---...',
+    ';': '-.-.-.',
+    '_': '..--.-',
+    '$': '...-..-',
+    ' ': '/'
+};
+const strgToMorse = (strg) => {
+    if (strg) {
+        let morseCode = '';
+        for (let i = 0; i < strg.length; i++) {
+            let char = strg[i].toLowerCase();
+            if (morseCodeMap[char]) {
+                morseCode += morseCodeMap[char];
+                morseCode += ' ';
+            }
+        }
+        return morseCode.trim();
+    }
+    else {
+        return 'No input provided';
+    }
+};
+const morseCode = strgToMorse(input);
+console.log(input);
+console.log(morseCode);
